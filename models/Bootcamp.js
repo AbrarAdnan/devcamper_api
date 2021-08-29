3493f1badb328b7e89741f23df46668725c4c2e6
AbrarAdnan <abrar.adnan01@gmail.com>
2021-08-29 07:45:47 +0600

    5.6 Mongoose Middleware slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
 	{
@@ -113,4 +114,9 @@ const BootcampSchema = new mongoose.Schema(
 	// }
 );
 
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+	this.slug = slugify(this.name, { lower: true });
+	next();
+});
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
