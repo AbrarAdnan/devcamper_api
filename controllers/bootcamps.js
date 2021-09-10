5a9acf1e1b159c739ff9024fa54fff2fd0b788d4
AbrarAdnan <abrar.adnan01@gmail.com>
2021-09-10 16:01:31 +0600

    6.8 Populate, Virtuals, Cascade Delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -27,7 +27,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	);
 
 	// Finding resource
-	let query = Bootcamp.find(JSON.parse(queryStr));
+	let query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
 	// Select Fields
 	if (req.query.select) {
@@ -147,6 +147,9 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 			new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
 		);
 	}
+
+	bootcamp.remove();
+
 	res.status(200).json({
 		success: true,
 		data: null,
